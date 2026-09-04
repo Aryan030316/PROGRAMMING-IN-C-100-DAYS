f90bb11f3f90b1ca374bf6402dae0252d1a90b20
Aryan030316 <Aryan.33883@stu.upes.ac.in>
2026-09-04 18:30:00 +0530

    Day 26: Solved Q51 and Q52

---
+++ Day26/q52.c
@@ -0,0 +1,40 @@
+﻿/*
+Q52: Write a program to print the following pattern:
+
+*
+
+*
+*
+*
+
+*
+*
+*
+*
+*
+
+*
+*
+*
+
+*
+
+Sample Test Cases:
+Input 1:
+
+Output 1:
+Pattern with stars spaced irregularly as shown.
+*/
+
+#include <stdio.h>
+
+int main() {
+    int rows[] = {1, 3, 5, 3, 1};
+    for (int k = 0; k < 5; k++) {
+        for (int i = 0; i < rows[k]; i++) {
+            printf("*\n");
+        }
+        if (k < 4) printf("\n");
+    }
+    return 0;
+}
