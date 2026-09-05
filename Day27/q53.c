e1119f94bd5ee2e921e4ab1ed98070511669dc81
Aryan030316 <Aryan.33883@stu.upes.ac.in>
2026-09-05 18:30:00 +0530

    Day 27: Solved Q53 and Q54

---
+++ Day27/q53.c
@@ -0,0 +1,39 @@
+﻿/*
+Q53: Write a program to print the following pattern:
+*
+***
+*****
+*******
+*********
+*******
+*****
+***
+*
+
+Sample Test Cases:
+Input 1:
+
+Output 1:
+*
+***
+*****
+*******
+*********
+*******
+*****
+***
+*
+*/
+
+#include <stdio.h>
+
+int main() {
+    int counts[] = {1, 3, 5, 7, 9, 7, 5, 3, 1};
+    for (int i = 0; i < 9; i++) {
+        for (int j = 0; j < counts[i]; j++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
---
+++ Day27/q54.c
@@ -0,0 +1,34 @@
+﻿/*
+Q54: Write a program to print the following pattern:
+
+   *
+  ***
+ *****
+*******
+ *****
+  ***
+   *
+
+Sample Test Cases:
+Input 1:
+
+Output 1:
+Pattern with layers of stars as shown.
+*/
+
+#include <stdio.h>
+
+int main() {
+    int stars[]  = {1, 3, 5, 7, 5, 3, 1};
+    int spaces[] = {3, 2, 1, 0, 1, 2, 3};
+    for (int i = 0; i < 7; i++) {
+        for (int s = 0; s < spaces[i]; s++) {
+            printf(" ");
+        }
+        for (int j = 0; j < stars[i]; j++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
