65de200099980524ddbcf6004ee33ebfb96beaf4
Aryan030316 <Aryan.33883@stu.upes.ac.in>
2026-09-07 18:30:00 +0530

    Day 29: Solved Q57 and Q58

---
+++ Day29/q58.c
@@ -0,0 +1,34 @@
+﻿/*
+Q58: Find the maximum and minimum element in an array.
+
+Sample Test Cases:
+Input 1:
+5
+2 9 1 4 7
+Output 1:
+Max=9, Min=1
+
+Input 2:
+3
+10 10 10
+Output 2:
+Max=10, Min=10
+*/
+
+#include <stdio.h>
+
+int main() {
+    int n;
+    if (scanf("%d", &n) == 1 && n > 0) {
+        int max, min, val;
+        scanf("%d", &val);
+        max = min = val;
+        for (int i = 1; i < n; i++) {
+            scanf("%d", &val);
+            if (val > max) max = val;
+            if (val < min) min = val;
+        }
+        printf("Max=%d, Min=%d\n", max, min);
+    }
+    return 0;
+}
