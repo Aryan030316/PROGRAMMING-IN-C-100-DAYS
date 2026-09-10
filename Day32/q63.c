d9e92eb76650efd403bdb26d9ac8d90b71fa602f
Aryan030316 <Aryan.33883@stu.upes.ac.in>
2026-09-10 18:30:00 +0530

    Day 32: Solved Q63 and Q64

---
+++ Day32/q63.c
@@ -0,0 +1,31 @@
+﻿/*
+Q63: Merge two arrays.
+
+Sample Test Cases:
+Input 1:
+3
+1 2 3
+2
+4 5
+Output 1:
+1 2 3 4 5
+*/
+
+#include <stdio.h>
+
+int main() {
+    int n1, n2;
+    if (scanf("%d", &n1) == 1) {
+        int a[n1];
+        for (int i = 0; i < n1; i++) scanf("%d", &a[i]);
+        if (scanf("%d", &n2) == 1) {
+            int b[n2];
+            for (int i = 0; i < n2; i++) scanf("%d", &b[i]);
+            for (int i = 0; i < n1; i++) printf("%d ", a[i]);
+            for (int i = 0; i < n2; i++) {
+                printf("%d%c", b[i], (i == n2 - 1 ? '\n' : ' '));
+            }
+        }
+    }
+    return 0;
+}
---
+++ Day32/q64.c
@@ -0,0 +1,38 @@
+﻿/*
+Q64: Find the digit that occurs the most times in an integer number.
+
+Sample Test Cases:
+Input 1:
+112233
+Output 1:
+1
+
+Input 2:
+887799
+Output 2:
+7
+*/
+
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char s[100];
+    if (scanf("%s", s) == 1) {
+        int counts[10] = {0};
+        for (int i = 0; s[i] != '\0'; i++) {
+            if (s[i] >= '0' && s[i] <= '9') {
+                counts[s[i] - '0']++;
+            }
+        }
+        int maxCount = 0, bestDigit = 0;
+        for (int d = 0; d < 10; d++) {
+            if (counts[d] > maxCount) {
+                maxCount = counts[d];
+                bestDigit = d;
+            }
+        }
+        printf("%d\n", bestDigit);
+    }
+    return 0;
+}
