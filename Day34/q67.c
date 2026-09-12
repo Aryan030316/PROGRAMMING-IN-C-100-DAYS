16589351d4e65ebfe95afe5dca254395572b6fa2
Aryan030316 <Aryan.33883@stu.upes.ac.in>
2026-09-12 18:30:00 +0530

    Day 34: Solved Q67 and Q68

---
+++ Day34/q67.c
@@ -0,0 +1,32 @@
+﻿/*
+Q67: Insert an element in an array at a given position.
+
+Sample Test Cases:
+Input 1:
+4
+10 20 30 40
+2 15
+Output 1:
+10 20 15 30 40
+*/
+
+#include <stdio.h>
+
+int main() {
+    int n;
+    if (scanf("%d", &n) == 1) {
+        int a[n + 1];
+        for (int i = 0; i < n; i++) scanf("%d", &a[i]);
+        int pos, val;
+        if (scanf("%d %d", &pos, &val) == 2) {
+            for (int i = n; i > pos; i--) {
+                a[i] = a[i - 1];
+            }
+            a[pos] = val;
+            for (int i = 0; i <= n; i++) {
+                printf("%d%c", a[i], (i == n ? '\n' : ' '));
+            }
+        }
+    }
+    return 0;
+}
---
+++ Day34/q68.c
@@ -0,0 +1,31 @@
+﻿/*
+Q68: Delete an element from an array.
+
+Sample Test Cases:
+Input 1:
+5
+1 2 3 4 5
+2
+Output 1:
+1 2 4 5
+*/
+
+#include <stdio.h>
+
+int main() {
+    int n;
+    if (scanf("%d", &n) == 1) {
+        int a[n];
+        for (int i = 0; i < n; i++) scanf("%d", &a[i]);
+        int pos;
+        if (scanf("%d", &pos) == 1) {
+            for (int i = pos; i < n - 1; i++) {
+                a[i] = a[i + 1];
+            }
+            for (int i = 0; i < n - 1; i++) {
+                printf("%d%c", a[i], (i == n - 2 ? '\n' : ' '));
+            }
+        }
+    }
+    return 0;
+}
