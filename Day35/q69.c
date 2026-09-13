94427eca56f98327723d735e7a1abef65334e722
Aryan030316 <Aryan.33883@stu.upes.ac.in>
2026-09-13 18:30:00 +0530

    Day 35: Solved Q69 and Q70

---
+++ Day35/q69.c
@@ -0,0 +1,35 @@
+﻿/*
+Q69: Find the second largest element in an array.
+
+Sample Test Cases:
+Input 1:
+5
+10 20 30 40 50
+Output 1:
+40
+*/
+
+#include <stdio.h>
+#include <limits.h>
+
+int main() {
+    int n;
+    if (scanf("%d", &n) == 1 && n >= 2) {
+        int first = INT_MIN, second = INT_MIN;
+        for (int i = 0; i < n; i++) {
+            int val;
+            scanf("%d", &val);
+            if (val > first) {
+                second = first;
+                first = val;
+            } else if (val > second && val != first) {
+                second = val;
+            }
+        }
+        if (second != INT_MIN)
+            printf("%d\n", second);
+        else
+            printf("%d\n", first);
+    }
+    return 0;
+}
---
+++ Day35/q70.c
@@ -0,0 +1,33 @@
+﻿/*
+Q70: Rotate an array to the right by k positions.
+
+Sample Test Cases:
+Input 1:
+5
+1 2 3 4 5
+2
+Output 1:
+4 5 1 2 3
+*/
+
+#include <stdio.h>
+
+int main() {
+    int n;
+    if (scanf("%d", &n) == 1) {
+        int a[n];
+        for (int i = 0; i < n; i++) scanf("%d", &a[i]);
+        int k;
+        if (scanf("%d", &k) == 1) {
+            k = k % n;
+            int res[n];
+            for (int i = 0; i < n; i++) {
+                res[(i + k) % n] = a[i];
+            }
+            for (int i = 0; i < n; i++) {
+                printf("%d%c", res[i], (i == n - 1 ? '\n' : ' '));
+            }
+        }
+    }
+    return 0;
+}
