c5eddbb3fc4f8b60a101b26f5ce6ba15cd38aae3
Aryan030316 <Aryan.33883@stu.upes.ac.in>
2026-09-15 18:30:00 +0530

    Day 37: Solved Q73 and Q74

---
+++ Day37/q73.c
@@ -0,0 +1,32 @@
+﻿/*
+Q73: Find the sum of each row of a matrix and store it in an array.
+
+Sample Test Cases:
+Input 1:
+2 3
+1 2 3
+4 5 6
+Output 1:
+6 15
+*/
+
+#include <stdio.h>
+
+int main() {
+    int r, c;
+    if (scanf("%d %d", &r, &c) == 2) {
+        int rowSums[r];
+        for (int i = 0; i < r; i++) {
+            rowSums[i] = 0;
+            for (int j = 0; j < c; j++) {
+                int val;
+                scanf("%d", &val);
+                rowSums[i] += val;
+            }
+        }
+        for (int i = 0; i < r; i++) {
+            printf("%d%c", rowSums[i], (i == r - 1 ? '\n' : ' '));
+        }
+    }
+    return 0;
+}
