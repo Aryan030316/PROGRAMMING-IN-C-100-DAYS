bfc0b6c8a66aa10a15d97e8b48ae6207f71a397f
Aryan030316 <Aryan.33883@stu.upes.ac.in>
2026-09-16 18:30:00 +0530

    Day 38: Solved Q75 and Q76

---
+++ Day38/q76.c
@@ -0,0 +1,47 @@
+﻿/*
+Q76: Check if a matrix is symmetric.
+
+Sample Test Cases:
+Input 1:
+2 2
+1 2
+2 1
+Output 1:
+True
+
+Input 2:
+2 2
+1 0
+2 1
+Output 2:
+False
+*/
+
+#include <stdio.h>
+
+int main() {
+    int r, c;
+    if (scanf("%d %d", &r, &c) == 2) {
+        int m[r][c];
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) scanf("%d", &m[i][j]);
+        }
+        if (r != c) {
+            printf("False\n");
+            return 0;
+        }
+        int sym = 1;
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+                if (m[i][j] != m[j][i]) {
+                    sym = 0;
+                    break;
+                }
+            }
+            if (!sym) break;
+        }
+        if (sym) printf("True\n");
+        else printf("False\n");
+    }
+    return 0;
+}
