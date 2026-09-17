f70fe5ad81a166cfed6c987adbaacea9e3eaf625
Aryan030316 <Aryan.33883@stu.upes.ac.in>
2026-09-17 18:30:00 +0530

    Day 39: Solved Q77 and Q78

---
+++ Day39/q77.c
@@ -0,0 +1,50 @@
+﻿/*
+Q77: Check if the elements on the diagonal of a matrix are distinct.
+
+Sample Test Cases:
+Input 1:
+3 3
+1 2 3
+4 5 6
+7 8 1
+Output 1:
+False
+
+Input 2:
+3 3
+1 2 3
+4 5 6
+7 8 9
+Output 2:
+True
+*/
+
+#include <stdio.h>
+
+int main() {
+    int r, c;
+    if (scanf("%d %d", &r, &c) == 2) {
+        int diag[r < c ? r : c];
+        int diagLen = r < c ? r : c;
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+                int val;
+                scanf("%d", &val);
+                if (i == j) diag[i] = val;
+            }
+        }
+        int distinct = 1;
+        for (int i = 0; i < diagLen; i++) {
+            for (int j = i + 1; j < diagLen; j++) {
+                if (diag[i] == diag[j]) {
+                    distinct = 0;
+                    break;
+                }
+            }
+            if (!distinct) break;
+        }
+        if (distinct) printf("True\n");
+        else printf("False\n");
+    }
+    return 0;
+}
---
+++ Day39/q78.c
@@ -0,0 +1,30 @@
+﻿/*
+Q78: Find the sum of main diagonal elements for a square matrix.
+
+Sample Test Cases:
+Input 1:
+3 3
+1 2 3
+4 5 6
+7 8 9
+Output 1:
+15
+*/
+
+#include <stdio.h>
+
+int main() {
+    int r, c;
+    if (scanf("%d %d", &r, &c) == 2) {
+        long long sum = 0;
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+                int val;
+                scanf("%d", &val);
+                if (i == j) sum += val;
+            }
+        }
+        printf("%lld\n", sum);
+    }
+    return 0;
+}
