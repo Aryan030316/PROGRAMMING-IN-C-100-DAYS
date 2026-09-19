1e512a509c316ac9ad472fb7a0b77231c63a1056
Aryan030316 <Aryan.33883@stu.upes.ac.in>
2026-09-19 18:30:00 +0530

    Day 41: Solved Q81 and Q82

---
+++ Day41/q81.c
@@ -0,0 +1,28 @@
+﻿/*
+Q81: Count characters in a string without using built-in length functions.
+
+Sample Test Cases:
+Input 1:
+Hello
+Output 1:
+5
+
+Input 2:
+ 
+Output 2:
+1
+*/
+
+#include <stdio.h>
+
+int main() {
+    char s[500];
+    if (fgets(s, sizeof(s), stdin)) {
+        int len = 0;
+        while (s[len] != '\0' && s[len] != '\n' && s[len] != '\r') {
+            len++;
+        }
+        printf("%d\n", len);
+    }
+    return 0;
+}
---
+++ Day41/q82.c
@@ -0,0 +1,22 @@
+﻿/*
+Q82: Print each character of a string on a new line.
+
+Sample Test Cases:
+Input 1:
+Hi
+Output 1:
+H
+i
+*/
+
+#include <stdio.h>
+
+int main() {
+    char s[500];
+    if (fgets(s, sizeof(s), stdin)) {
+        for (int i = 0; s[i] != '\0' && s[i] != '\n' && s[i] != '\r'; i++) {
+            printf("%c\n", s[i]);
+        }
+    }
+    return 0;
+}
