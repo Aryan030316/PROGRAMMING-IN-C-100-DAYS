d51f85d5ee6acc1ccc0d15c10d3fe104ecdb087c
Aryan030316 <Aryan.33883@stu.upes.ac.in>
2026-09-21 18:30:00 +0530

    Day 43: Solved Q85 and Q86

---
+++ Day43/q85.c
@@ -0,0 +1,25 @@
+﻿/*
+Q85: Reverse a string.
+
+Sample Test Cases:
+Input 1:
+abcd
+Output 1:
+dcba
+*/
+
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char s[500];
+    if (fgets(s, sizeof(s), stdin)) {
+        int len = 0;
+        while (s[len] != '\0' && s[len] != '\n' && s[len] != '\r') len++;
+        for (int i = len - 1; i >= 0; i--) {
+            putchar(s[i]);
+        }
+        putchar('\n');
+    }
+    return 0;
+}
