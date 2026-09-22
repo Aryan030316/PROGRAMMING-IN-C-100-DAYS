96a03cd7d3569a5ffa221f83f3c467cc41def7c8
Aryan030316 <Aryan.33883@stu.upes.ac.in>
2026-09-22 18:30:00 +0530

    Day 44: Solved Q87 and Q88

---
+++ Day44/q87.c
@@ -0,0 +1,29 @@
+﻿/*
+Q87: Count spaces, digits, and special characters in a string.
+
+Sample Test Cases:
+Input 1:
+a b1&2
+Output 1:
+Spaces=1, Digits=2, Special=1
+*/
+
+#include <stdio.h>
+
+int main() {
+    char s[500];
+    if (fgets(s, sizeof(s), stdin)) {
+        int sp = 0, dig = 0, spec = 0;
+        for (int i = 0; s[i] != '\0' && s[i] != '\n' && s[i] != '\r'; i++) {
+            if (s[i] == ' ') sp++;
+            else if (s[i] >= '0' && s[i] <= '9') dig++;
+            else if ((s[i] >= 'a' && s[i] <= 'z') || (s[i] >= 'A' && s[i] <= 'Z')) {
+                // Alphabet
+            } else {
+                spec++;
+            }
+        }
+        printf("Spaces=%d, Digits=%d, Special=%d\n", sp, dig, spec);
+    }
+    return 0;
+}
---
+++ Day44/q88.c
@@ -0,0 +1,23 @@
+﻿/*
+Q88: Replace spaces with hyphens in a string.
+
+Sample Test Cases:
+Input 1:
+hello world
+Output 1:
+hello-world
+*/
+
+#include <stdio.h>
+
+int main() {
+    char s[500];
+    if (fgets(s, sizeof(s), stdin)) {
+        for (int i = 0; s[i] != '\0' && s[i] != '\n' && s[i] != '\r'; i++) {
+            if (s[i] == ' ') putchar('-');
+            else putchar(s[i]);
+        }
+        putchar('\n');
+    }
+    return 0;
+}
