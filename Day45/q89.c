9dd40aab3c126af8281a78eb327b39643012312d
Aryan030316 <Aryan.33883@stu.upes.ac.in>
2026-09-23 18:30:00 +0530

    Day 45: Solved Q89 and Q90

---
+++ Day45/q89.c
@@ -0,0 +1,27 @@
+﻿/*
+Q89: Count frequency of a given character in a string.
+
+Sample Test Cases:
+Input 1:
+programming
+g
+Output 1:
+2
+*/
+
+#include <stdio.h>
+
+int main() {
+    char s[500];
+    char ch;
+    if (scanf("%s", s) == 1) {
+        if (scanf(" %c", &ch) == 1) {
+            int freq = 0;
+            for (int i = 0; s[i] != '\0'; i++) {
+                if (s[i] == ch) freq++;
+            }
+            printf("%d\n", freq);
+        }
+    }
+    return 0;
+}
---
+++ Day45/q90.c
@@ -0,0 +1,24 @@
+﻿/*
+Q90: Toggle case of each character in a string.
+
+Sample Test Cases:
+Input 1:
+Hello
+Output 1:
+hELLO
+*/
+
+#include <stdio.h>
+
+int main() {
+    char s[500];
+    if (fgets(s, sizeof(s), stdin)) {
+        for (int i = 0; s[i] != '\0' && s[i] != '\n' && s[i] != '\r'; i++) {
+            if (s[i] >= 'a' && s[i] <= 'z') putchar(s[i] - ('a' - 'A'));
+            else if (s[i] >= 'A' && s[i] <= 'Z') putchar(s[i] + ('a' - 'A'));
+            else putchar(s[i]);
+        }
+        putchar('\n');
+    }
+    return 0;
+}
