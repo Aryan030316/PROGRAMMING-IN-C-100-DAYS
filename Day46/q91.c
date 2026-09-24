5952f7368fec1f45e70ba868ca150788d9dde07b
Aryan030316 <Aryan.33883@stu.upes.ac.in>
2026-09-24 18:30:00 +0530

    Day 46: Solved Q91 and Q92

---
+++ Day46/q91.c
@@ -0,0 +1,26 @@
+﻿/*
+Q91: Remove all vowels from a string.
+
+Sample Test Cases:
+Input 1:
+education
+Output 1:
+dctn
+*/
+
+#include <stdio.h>
+#include <ctype.h>
+
+int main() {
+    char s[500];
+    if (fgets(s, sizeof(s), stdin)) {
+        for (int i = 0; s[i] != '\0' && s[i] != '\n' && s[i] != '\r'; i++) {
+            char lower = tolower(s[i]);
+            if (lower != 'a' && lower != 'e' && lower != 'i' && lower != 'o' && lower != 'u') {
+                putchar(s[i]);
+            }
+        }
+        putchar('\n');
+    }
+    return 0;
+}
---
+++ Day46/q92.c
@@ -0,0 +1,36 @@
+﻿/*
+Q92: Find the first repeating lowercase alphabet in a string.
+
+Sample Test Cases:
+Input 1:
+stress
+Output 1:
+s
+*/
+
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char s[500];
+    if (scanf("%s", s) == 1) {
+        int count[26] = {0};
+        for (int i = 0; s[i] != '\0'; i++) {
+            if (s[i] >= 'a' && s[i] <= 'z') {
+                count[s[i] - 'a']++;
+            }
+        }
+        char firstRep = '\0';
+        for (int i = 0; s[i] != '\0'; i++) {
+            if (s[i] >= 'a' && s[i] <= 'z' && count[s[i] - 'a'] > 1) {
+                firstRep = s[i];
+                break;
+            }
+        }
+        if (firstRep != '\0')
+            printf("%c\n", firstRep);
+        else
+            printf("None\n");
+    }
+    return 0;
+}
