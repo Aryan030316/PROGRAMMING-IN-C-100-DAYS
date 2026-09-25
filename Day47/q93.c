2a32a65b712d467c6328e2370e26f9228333dea4
Aryan030316 <Aryan.33883@stu.upes.ac.in>
2026-09-25 18:30:00 +0530

    Day 47: Solved Q93 and Q94

---
+++ Day47/q93.c
@@ -0,0 +1,45 @@
+﻿/*
+Q93: Check if two strings are anagrams of each other.
+
+Sample Test Cases:
+Input 1:
+listen
+silent
+Output 1:
+Anagrams
+
+Input 2:
+hello
+world
+Output 2:
+Not anagrams
+*/
+
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+int main() {
+    char s1[500], s2[500];
+    if (scanf("%s", s1) == 1 && scanf("%s", s2) == 1) {
+        if (strlen(s1) != strlen(s2)) {
+            printf("Not anagrams\n");
+            return 0;
+        }
+        int freq[256] = {0};
+        for (int i = 0; s1[i] != '\0'; i++) {
+            freq[(unsigned char)tolower(s1[i])]++;
+            freq[(unsigned char)tolower(s2[i])]--;
+        }
+        int isAnagram = 1;
+        for (int i = 0; i < 256; i++) {
+            if (freq[i] != 0) {
+                isAnagram = 0;
+                break;
+            }
+        }
+        if (isAnagram) printf("Anagrams\n");
+        else printf("Not anagrams\n");
+    }
+    return 0;
+}
---
+++ Day47/q94.c
@@ -0,0 +1,39 @@
+﻿/*
+Q94: Find the longest word in a sentence.
+
+Sample Test Cases:
+Input 1:
+I love programming
+Output 1:
+programming
+*/
+
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char s[1000];
+    if (fgets(s, sizeof(s), stdin)) {
+        char longest[200] = """";
+        int maxLen = 0;
+        char current[200] = """";
+        int curLen = 0;
+        
+        for (int i = 0; ; i++) {
+            char ch = s[i];
+            if (ch == ' ' || ch == '\n' || ch == '\r' || ch == '\0') {
+                if (curLen > maxLen) {
+                    maxLen = curLen;
+                    current[curLen] = '\0';
+                    strcpy(longest, current);
+                }
+                curLen = 0;
+                if (ch == '\0') break;
+            } else {
+                current[curLen++] = ch;
+            }
+        }
+        printf("%s\n", longest);
+    }
+    return 0;
+}
